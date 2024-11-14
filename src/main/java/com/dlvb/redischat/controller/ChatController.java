9b8e8cbb8dc0e150ce1ac3863523cff227ac917d
Антон Матушкин <matushkin-2017@mail.ru>
2024-11-14 16:40:37 +0500

    feat: add controller for chat

---
+++ src/main/java/com/dlvb/redischat/controller/ChatController.java
@@ -0,0 +1,18 @@
+package com.dlvb.redischat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+    @GetMapping
+    public String homePage() {
+        return "index";
+    }
+
+    @GetMapping("/chat")
+    public String chatPage() {
+        return "chat";
+    }
+}
