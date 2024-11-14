09b411c0a46d4ad5d99f699ae55f51953d6d55f0
Антон Матушкин <matushkin-2017@mail.ru>
2024-11-14 16:37:12 +0500

    feat: add session manager for websockets

---
+++ src/main/java/com/dlvb/redischat/websocket/WebSocketSessionManager.java
@@ -0,0 +1,32 @@
+package com.dlvb.redischat.websocket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class WebSocketSessionManager {
+    private final Map<String, WebSocketSession> webSocketSessionByUserId = new HashMap<>();
+
+    public void addWebSocketSession(WebSocketSession webSocketSession){
+        String userId = WebSocketHelper.getUserIdFromSessionAttribute(webSocketSession);
+        log.info("Got request to add session id {} for user id {} ", webSocketSession.getId(), userId);
+        webSocketSessionByUserId.put(userId,webSocketSession);
+        log.info("Added session id {} for user id {}", webSocketSession.getId(), userId);
+    }
+
+    public void removeWebSocketSession(WebSocketSession webSocketSession){
+        String userId = WebSocketHelper.getUserIdFromSessionAttribute(webSocketSession);
+        log.info("Got request to remove session id {} for user id {}", webSocketSession.getId(), userId);
+        webSocketSessionByUserId.remove(userId);
+        log.info("Removed session id {} for user id {}", webSocketSession.getId(), userId);
+    }
+
+    public WebSocketSession getWebSocketSessions(String userId){
+        return this.webSocketSessionByUserId.get(userId);
+    }
+}
